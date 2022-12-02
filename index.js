0817ad60078b1211a62b88cb4aeae8ab3c3eecd3
FelipeMacedo315 <felipemacedo315@gmail.com>
2022-12-02 13:19:11 -0300

    add melhorias no design

--- index.js
+++ index.js
@@ -7,20 +7,27 @@ const inputPasswordField = document.getElementById("password");
 function validateData() {
   const email = document.getElementsByName("email")[0].value;
   const password = document.getElementsByName("password")[0].value;
+  var inputFieldEmail = document.getElementsByTagName("input")[0];
+  var inputFieldPassword = document.getElementsByTagName("input")[1];
   if (email.length === 0) {
+    inputFieldEmail.className = "input-error";
     document.getElementsByTagName("small")[0].innerText = "E necessário informar um E-mail";
-  } else if (!email.includes("@") && !email.includes(".com")) {
+  } else if (!email.includes("@") || !email.includes(".com")) {
+    inputFieldEmail.className = "input-error";
     document.getElementsByTagName("small")[0].innerText = "informe um email valido";
   } else {
+    inputFieldEmail.className = "input-ok";
     document.getElementsByTagName("small")[0].innerText = "";
   }
 
   if (password.length === 0) {
+    inputFieldPassword.className = "input-error";
     document.getElementsByTagName("small")[1].innerText = "E necessário informar uma senha ";
     inputPasswordIcon.className = "icon-input-error";
   } else {
     document.getElementsByTagName("small")[1].innerText = "";
     inputPasswordIcon.className = "icon-input-ok";
+    inputFieldPassword.className = "input-ok";
   }
 }
 
